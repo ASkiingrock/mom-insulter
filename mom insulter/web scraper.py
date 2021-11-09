70f6efeaa190eedb103ba571e1bc964bc670a6d6
O S <o.p.smith@icloud.com>
2021-11-09 15:33:50 +1100

    check comments

--- mom insulter/web scraper.py
+++ mom insulter/web scraper.py
@@ -1,19 +1,37 @@
-import requests
-from bs4 import BeautifulSoup
+#wasn't bothered to build a web scraper
+#just copied yo mama jokes from a website, but there were new lines characters so JSON didn't like it
+#imported it into word, ctrl h and then select paragraph mark, replace all
+#do the same, and then manual new line, replace all
+#then its just a solid slab of text
 
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
+import re
+import json
+with open ("test.json", "r") as file:
+    yomamas = json.load(file)
 
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="ResultsContainer")
-job_elements = results.find_all("div", class_="card-content")
-for item in job_elements:
-    '''title_element = item.find("h2", class_="title")
-    company_element = item.find("h3", class_="company")
-    location_element = item.find("p", class_="location")
-    print(title_element.text.strip())
-    print(company_element.text.strip())
-    print(location_element.text.strip())
-    print()'''
-    python_jobs = results.find_all("h2", string=lambda text: "python" in text.lower())
-    print(python_jobs)
+#split into sentences
+yomamas = str(yomamas)
+yomamas = ''.join([i for i in yomamas if not i.isdigit()])
+sentences = re.split(r'(?<=[^A-Z].[.?]) +(?=[A-Z])', yomamas)
+
+#split into yo mama's so... (a[0]) and then insult (a[2])
+for item in sentences:
+    if "Yo mama so" not in item:
+        sentences.remove(item)
+    else: 
+        saysplit = item.split("Yo mama so",1)
+        saysplit = saysplit[1][1:-1]
+        a = str(saysplit).partition(',')
+        category = a[0]
+        insult = a[2]
+#this doesn't work yet
+        with open ("test.json", "r") as file:
+            yomamas = json.load(file)
+            if category.lower() in "olddumbtallshortrichpoorother":
+                json.update({(category.lower()):insult})
+            else:
+                json.update({"other":a})
+
+        
+        
+
