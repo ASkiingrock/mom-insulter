12351e2a66abea8ce1777271f40f092c0a2e3a83
O S <o.p.smith@icloud.com>
2021-11-15 18:47:36 +1100

    added some more of the good stuff

--- joke-reading/joke_reader.py
+++ joke-reading/joke_reader.py
@@ -3,15 +3,23 @@ import json
 # Read the jokes from the raw jokes file. Gets each line as a list
 with open("joke-reading/raw_jokes.txt", "r", encoding="utf-8") as file:
     lines = file.readlines()
-    file.close()
+
+with open("insults.json", "r", encoding="utf-8") as file:
+    length = 0
+    for line in file:
+        length+=1
+
 
 for line in lines:
-    if "Yo mama" not in line:  # Remove if you mama isn't in the joke
+    # edit this based on format of text (e.g. starts with yo mama or yo mama's or yo mammy) or else it won't do anything
+    if "Yo Mama" not in line:  # Remove if you mama isn't in the joke
         lines.remove(line)
     else:
         # Splits up the joke into category and punchline
         try:
+            # may need to change depending on format
             split_yo_mama = line.split("Yo Mama so ")
+            # returns errors if phrase is not in
             split_joke = split_yo_mama[1].split(", ", 1)
             category = split_joke[0].lower()  # e.g. fat, ugly, old, short
             punchline = split_joke[1].strip()  # What comes after the above
@@ -35,9 +43,16 @@ for line in lines:
             with open("insults.json", "w", encoding="utf-8") as file:
                 json.dump(insults, file, indent=4, ensure_ascii=False)
                 file.close()
-        except IndexError:
-            print("indexerror")
+        except Exception as e:
+            print(f"\"{line}\" not correct format ({e})")
             continue
+with open("insults.json", "r", encoding="utf-8") as file:
+    nlength = 0
+    for line in file:
+        nlength+=1
+    print(f'{nlength - length} lines were added')
+    file.close()
+                
         
 
 
